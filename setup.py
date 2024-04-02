decb94f3f4878f3e31f306405c2d979abc622ec7
Dongjun Guo <dongjung@kakus-MacBook-Air.local>
2024-04-02 20:57:31 +0100

    update the code to install anarci_vc

--- setup.py
+++ setup.py
@@ -7,58 +7,57 @@
 
 # Modified by Dongjun Guo to coorperate with anarci_vc #
 
-import shutil, os, subprocess, importlib
-# Clean this out if it exists
-if os.path.isdir("build"):
-    shutil.rmtree("build/")
-
+import shutil, os, subprocess
+import site, sys
+from importlib import util
 from distutils.core import setup
+from setuptools.command.install import install
+
+class CustomInstallCommand(install):
+   def run(self):
+       install.run(self)
+       # Post-installation routine
+       ANARCI_LOC = os.path.join(site.getsitepackages()[0], 'anarci_vc') # site-packages/ folder
+       ANARCI_BIN = sys.executable.split('python')[0] # bin/ folder
+
+       shutil.copy('bin/ANARCI', ANARCI_BIN) # copy ANARCI executable
+       print("INFO: ANARCI_vc lives in: ", ANARCI_LOC) 
+
+       # Build HMMs from IMGT germlines
+       os.chdir("build_pipeline")
+       print('INFO: Downloading germlines from IMGT and building HMMs...')
+       print("INFO: running 'RUN_pipeline.sh', this will take a couple a minutes.")
+       proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+       o, e = proc.communicate()
+
+       print(o.decode())
+       print(e.decode())
+       
+       # Copy HMMs where ANARCI can find them
+       shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
+       os.mkdir(os.path.join(ANARCI_LOC, "dat"))
+       shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
+      
+      # Remove data from HMMs generation
+       try:
+           shutil.rmtree("curated_alignments/")
+           shutil.rmtree("muscle_alignments/")
+           shutil.rmtree("HMMs/")
+           shutil.rmtree("IMGT_sequence_files/")
+       except OSError:
+           pass
 
 setup(name='anarci_vc',
-      version='1.3',
-      description='Antibody Numbering and Receptor ClassIfication',
-      author='James Dunbar',
-      author_email='opig@stats.ox.ac.uk',
-      url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
-      packages=['anarci_vc'], 
-      package_dir={'anarci_vc': 'lib/python/anarci_vc'},
-      #package_data={'anarci_vc': ['dat/HMMs/ALL.hmm',
-      #                         'dat/HMMs/ALL.hmm.h3f',
-      #                         'dat/HMMs/ALL.hmm.h3i',
-      #                         'dat/HMMs/ALL.hmm.h3m',
-      #                         'dat/HMMs/ALL.hmm.h3p']},
-      scripts=['bin/ANARCI'],
-      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ]
-     )
+     version='1.3',
+     description='Antibody Numbering and Receptor ClassIfication',
+     author='James Dunbar',
+     author_email='opig@stats.ox.ac.uk',
+     url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
+     packages=['anarci_vc'],
+     package_dir={'anarci_vc': 'lib/python/anarci_vc'},
+     data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS', 'bin/ANARCI']) ],
+     include_package_data = True,
+     scripts=['bin/ANARCI'],
+     cmdclass={"install": CustomInstallCommand, }, # Run post-installation routine
+    )
 
-####
-import sys
-if sys.argv[1] != "install":
-    sys.exit(0)
-
-try:
-    ANARCI_LOC = importlib.util.find_spec("anarci_vc").submodule_search_locations[0]
-except Exception as e:
-    sys.stderr.write(e.message)
-    sys.exit(1)
-
-os.chdir("build_pipeline")
-
-try:
-    shutil.rmtree("curated_alignments/")
-    shutil.rmtree("muscle_alignments/")
-    shutil.rmtree("HMMs/")
-    shutil.rmtree("IMGT_sequence_files/")
-    os.mkdir(os.path.join(ANARCI_LOC, "dat"))
-except OSError:
-    pass
-
-print('Downloading germlines from IMGT and building HMMs...')
-proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-o, e = proc.communicate()
-
-print(o.decode())
-print(e.decode())
-
-shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
